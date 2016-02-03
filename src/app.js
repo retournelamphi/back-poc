a710bbff23af8a818698a2bfda12c2818c20c968
Louis Lainé <darksioul6@gmail.com>
2016-02-03 15:43:34 +0100

    feat(*): Hello World bootstrap + webpack prod config

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => console.log('Express app running on port 3000'));
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,12 +1,23 @@
 var path = require('path');
 var webpack = require('webpack');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+    .filter(function(x) {
+      return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function(mod) {
+      nodeModules[mod] = 'commonjs ' + mod;
+    });
+
+
 
 module.exports = {
-  entry: [
-    './src/server.js'
-  ],
+  entry: './src/app.js',
+  target: 'node',
+  externals: nodeModules,
   output: {
-    path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
   resolve: {
