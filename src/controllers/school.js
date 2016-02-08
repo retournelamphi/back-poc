5e0b0076d4ac6b1131ff84de4ba67092ba59d3c5
Jérémy Morin <hi@jermor.in>
2016-02-08 10:42:53 +0100

    feat(school) add post and delete method

--- src/controllers/school.js
+++ src/controllers/school.js
@@ -6,18 +6,47 @@ export function getOne(req, res, next, id) {
     }).catch(e => next(e));
 }
 
-export function list(req, res, next){
+export function list(req, res, next) {
     const { limit = 50, skip = 0 } = req.query;
-    School.list({ limit, skip }).then(schools => res.json(schools)).catch(e => next(e));
+    School.list({limit, skip}).then(schools => res.json(schools)).catch(e => next(e));
 }
 
 export function query(req, res, next) {
-  const queryText = req.query.text;
-  School.searchOnName(queryText)
-    .then(results => {
-      res.send(results)
-    })
-    .catch(err => {
-      next(err);
+    const queryText = req.query.text;
+    School.searchOnName(queryText)
+        .then(results => {
+            res.send(results)
+        })
+        .catch(err => {
+            next(err);
+        });
+}
+
+export function create(req, res, next) {
+    const school = new School({
+        type: req.body.type,
+        name: req.body.name,
+        acronym: req.body.acronym,
+        status: req.body.status,
+        tutor: req.body.tutor,
+        university: req.body.university,
+        adress: req.body.adress,
+        zip: req.body.zip,
+        town: req.body.town,
+        department: req.body.department,
+        academy: req.body.academy,
+        region: req.body.region,
+        longitude: req.body.longitude,
+        latitude: req.body.latitude
     });
+
+    school.save()
+        .then(savedSchool => res.json(savedSchool))
+        .catch(e => next(e));
+}
+
+export function remove(req, res, next, id){
+        School.remove(id)
+            .then(deletedSchool => res.json(deletedSchool))
+            .catch(e => next(e));
 }
